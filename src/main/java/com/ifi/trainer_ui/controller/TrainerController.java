b431ca4d3d1fd9674c5285cbd47f16441f391067
Fournier Allan <fournier@b11p14.fil.univ-lille1.fr>
2019-12-17 13:11:50 +0100

    Rendu final V2

--- src/main/java/com/ifi/trainer_ui/controller/TrainerController.java
+++ src/main/java/com/ifi/trainer_ui/controller/TrainerController.java
@@ -1,15 +1,24 @@
 package com.ifi.trainer_ui.controller;
 
+import com.ifi.trainer_ui.pokemonTypes.service.PokemonTypeService;
+import com.ifi.trainer_ui.trainers.bo.Trainer;
 import com.ifi.trainer_ui.trainers.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class TrainerController {
 
+    private PokemonTypeService pokemonTypeService;
 
     private TrainerService trainerService;
 
@@ -36,8 +45,10 @@ public class TrainerController {
 
     @GetMapping("/profile")
     ModelAndView profile(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
         var modelAndView = new ModelAndView("profile");
-        modelAndView.addObject("trainer", trainerService.getAllTrainers());
+        modelAndView.addObject("trainer", trainerService.getTrainer(user.getUsername()));
         return modelAndView;
     }
 
